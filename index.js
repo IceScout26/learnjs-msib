1352c5e56e21f5fa7a3af91b8a67d94cd5e82b48
IceScout26 <dhimasscout@gmail.com>
2023-09-03 22:43:19 +0700

    reupload

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-//dhimas
 const dollar1 = 10
 const dollar2 = 2
 const isMember = true
-let money = null
+const money = null
 
 if (isMember) {
     money = dollar1
-    console.log(money)
 } else {
     money = dollar2
-    console.log(money)
 }
